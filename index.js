4108fd27abdc968e1240dc5af1a29736838e4cc0
Tatiana <tatiana@tatiana-ThinkPad-R61.(none)>
2016-09-20 02:11:21 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+var fs = require('fs');
+var nodemailer = require('nodemailer');
+var config = require('node-config');
+var filepath = config.filepath;
+
+var smtpConfig = {
+    host: config.emailHost,
+    secure: config.emailSecure, 					                // use SSL
+    auth: {
+        user: config.emailUser,
+        pass: config.emailPass
+    }
+};
+
+var transporter = nodemailer.createTransport(smtpConfig);
+
+// setup e-mail data
+var mailOptions = {
+    from: config.emailFrom,   // sender address
+    to: config.emailTo, 				                    // list of receivers
+    subject: config.emailSubject, 			                  // Subject line
+    text: config.emailText, 				                      // plaintext body
+    attachments: [
+	{
+	  path: filepath	
+	}
+    ]
+};
+
+fs.watchFile(filepath, { persistent: true, interval: 10000 }, function(curr, prev) {
+  console.log('image was modified');
+  
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+  });	
+});
+
